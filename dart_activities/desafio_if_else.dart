fe6c020c6037adf8f120103c5070507059dc66f4
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2023-02-20 15:24:13 -0300

    Add dart_activities and questions_project

---
+++ dart_activities/desafio_if_else.dart
@@ -0,0 +1,10 @@
+main() {
+  var nota = 2.3;
+
+  if (nota >= 9.0) ;
+  {
+    print('Parabéns! Você foi brilhante');
+  }
+}
+// O print sera executado pois o if executara a proxima linha de comando, que 
+// no caso e vazia. Uma linha de comando e finalizada por ';'.
---
+++ dart_activities/if_else.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+main() {
+  var nota = Random().nextInt(11);
+  print('Nota: $nota');
+
+  if (nota >= 9)
+    print('Quadro de Honra');
+  if (nota >= 7)
+    print('Aprovado!');
+  else if (nota >= 5)
+    print('Recuperação');
+  else if (nota >= 4)
+    print('Recuperação + Trabalho');
+  else
+    print('Reprovado');
+  print('Fim');
+}
