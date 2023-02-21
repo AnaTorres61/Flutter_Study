a1c08e280314eb4ac736f095ec5ca19b94454d63
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2023-02-20 22:31:16 -0300

    Update question_project

--- questions_project/lib/main.dart
+++ questions_project/lib/main.dart
@@ -1,68 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+main() => runApp(PerguntaApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void responder() {
     setState(() {
-      _counter++;
+      perguntaSelecionada++;
     });
+    print(perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+          backgroundColor: Color.fromARGB(255, 215, 184, 252),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+              onPressed: responder, 
+              child: Text('Resposta 1'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 215, 184, 252),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: responder, 
+              child: Text('Resposta 2'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 215, 184, 252),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: responder, 
+              child: Text('Resposta 3'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 215, 184, 252),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
